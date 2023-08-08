98783b84ab52f7d114ad4ef2f5919416f30c7044
Erféro KEOULA <erferokam@gmail.com>
2023-08-08 21:25:08 +0200

    Affichage conditionné de la page

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const menu = document.querySelector(".pots");
+    const Copy = document.getElementById("copywrite");
+    const Body = document.querySelector("body");
     const elementMenu = document.querySelector(".li");
     console.log(menu, elementMenu);
 
-    menu.addEventListener('click', ()=>{
+    menu.addEventListener('click', () => {
         console.log('clique menu');
         elementMenu.classList.toggle('block')
     })
+
+    if (!Copy) {
+        Body.innerHTML = " ";
+    }
 
 })
